e1fba502126bb90adb256de008b849b175049945
Rounik <rounik1020@gamil.com>
2022-04-01 15:08:31 +0530

    Created UserDetails model

--- app/src/main/java/com/project/bitereg/models/User.kt
+++ app/src/main/java/com/project/bitereg/models/User.kt
@@ -3,5 +3,6 @@ package com.project.bitereg.models
 data class User(
     val id: String = "",
     val name: String = "",
-    val email: String = ""
+    val email: String = "",
+    var details: UserDetails? = null
 )
---
+++ app/src/main/java/com/project/bitereg/models/UserDetails.kt
@@ -0,0 +1,10 @@
+package com.project.bitereg.models
+
+data class UserDetails(
+    val course: String,
+    val branch: String,
+    val batch: Int,
+    val dob: String,
+    val gender: String,
+    val phoneNumber: String
+)
